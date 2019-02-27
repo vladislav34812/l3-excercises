467bdce2a1aa4beebcd7a5c0210befbe1f07f574
vladislav34812 <47220745+vladislav34812@users.noreply.github.com>
2019-02-28 01:19:20 +0200

    Add files via upload

---
+++ Human/Circle/Circle.java
@@ -0,0 +1,24 @@
+package Circle;
+
+public class Circle {
+
+    public Circle(){
+
+    }
+    double radius;
+
+    public double getRadius(){
+        return radius;
+    }
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+    double calculateArea(){
+        return 3.14*radius*radius;
+    }
+    public double getCalclateArea(){
+        return 3.14*radius*radius;
+
+    }
+}
+
