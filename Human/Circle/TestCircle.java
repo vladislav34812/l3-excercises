5d2e218618499d11c4dd590f4716609f3236e270
vladislav34812 <47220745+vladislav34812@users.noreply.github.com>
2019-02-28 01:20:38 +0200

    Delete TestCircle.java

--- Human/Circle/TestCircle.java
+++
@@ -1,9 +0,0 @@
-package Circle;
-
-public class TestCircle {
-    public static void main(String[]args){
-        Circle circle1 = new Circle();
-        circle1.setRadius(6.5);
-    System.out.println("Площадь круга с радиусом " + circle1.getRadius()+ " см равна "+ circle1.getCalclateArea() + " см");
-    }
-}
